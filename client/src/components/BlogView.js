d952d5c44d5d096486e72d534aa5e08b9b1e8924
Steven Craig <stevencraig321@gmail.com>
2021-07-16 09:41:00 +0100

    share links working

--- client/src/components/BlogView.js
+++ client/src/components/BlogView.js
@@ -12,10 +12,11 @@ import {
   Tooltip,
 } from "@chakra-ui/react";
 
-import { FaPinterest, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 import DisplayBar from "./navbars/DisplayBar";
 import { categoryColor } from "../utils/categoryColor";
+import FadeInWhenVisible from "../utils/FadeInWhenVisible";
 
 const BlogView = ({ data }) => {
   const createContent = () => {
@@ -38,7 +39,7 @@ const BlogView = ({ data }) => {
           objectFit="cover"
           src={data.thumbnail}
         />
-        <VStack spacing={4} py={{ base: "20px", lg: "0" }}>
+        <VStack spacing={4} py={{ base: "20px", lg: "10px" }}>
           <Box px="10px" textStyle="excerpt" bg={categoryColor(data.category)}>
             {data.category}
           </Box>
@@ -77,58 +78,53 @@ const BlogView = ({ data }) => {
 };
 
 const ShareLinks = ({ data }) => {
-  const facebookLink = `https://www.facebook.com/sharer.php?u=http%3A%2F%2Fking-of.tech/${data.slug}`;
+  const facebookLink = `https://www.facebook.com/sharer.php?u=http%3A%2F%2Fking-of.tech%2Fblog%2F${data.slug}`;
 
-  const twitterLink = `https://twitter.com/intent/tweet
-  ?url=http%3A%2F%2Fking-of.tech%2F&text=${data.slug
-    .split(" ")
-    .join("+")}&hashtags=css,html`;
+  const twitterLink = `https://twitter.com/intent/tweet?text=https%3A//www.king-of.tech/blog/${data.slug}`;
 
-  const linkedinLink = `https://www.linkedin.com/shareArticle
-  ?mini=true
-  &url=https%3A%2F%2Fwww.king-of.tech%2F
-  &title=${data.slug}
-  &summary=&source=King-of-Tech`;
-
-  const pinterestLink = `https://pinterest.com/pin/create/button/?url=http://king-of.tech/blog/${data.slug}&media=&description=`;
+  const linkedinLink = `https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.king-of.tech/blog/${data.slug}&title=&summary=&source=`;
 
   return (
-    <VStack spacing={4}>
-      <h3>Liked it? Share it!</h3>
-      <HStack w="250px" justify="space-between" fontSize="35px">
-        <Tooltip hasArrow label="Share on Pinterest">
-          <span>
-            <a href={pinterestLink}>
-              <FaPinterest color="#e60023" cursor="pointer" />
-            </a>
-          </span>
-        </Tooltip>
+    <>
+      <FadeInWhenVisible>
+        <VStack spacing={4}>
+          <h3>Liked it? Share it!</h3>
+          <HStack w="250px" justify="space-between" fontSize="35px">
+            <Tooltip hasArrow label="Share on Facebook">
+              <span>
+                <a
+                  href={facebookLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaFacebook color="#4495d4" cursor="pointer" />
+                </a>
+              </span>
+            </Tooltip>
 
-        <Tooltip hasArrow label="Share on Facebook">
-          <span>
-            <a href={facebookLink}>
-              <FaFacebook color="#4495d4" cursor="pointer" />
-            </a>
-          </span>
-        </Tooltip>
+            <Tooltip hasArrow label="Share on Twitter">
+              <span>
+                <a href={twitterLink} target="_blank" rel="noopener noreferrer">
+                  <FaTwitter color="#1da1f2" cursor="pointer" />
+                </a>
+              </span>
+            </Tooltip>
 
-        <Tooltip hasArrow label="Share on Twitter">
-          <span>
-            <a href={twitterLink}>
-              <FaTwitter color="#1da1f2" cursor="pointer" />
-            </a>
-          </span>
-        </Tooltip>
-
-        <Tooltip hasArrow label="Share on Linkedin">
-          <span>
-            <a href={linkedinLink}>
-              <FaLinkedin color="#0a66c2" cursor="pointer" />
-            </a>
-          </span>
-        </Tooltip>
-      </HStack>
-    </VStack>
+            <Tooltip hasArrow label="Share on Linkedin">
+              <span>
+                <a
+                  href={linkedinLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin color="#0a66c2" cursor="pointer" />
+                </a>
+              </span>
+            </Tooltip>
+          </HStack>
+        </VStack>
+      </FadeInWhenVisible>
+    </>
   );
 };
 export default BlogView;
